018f2bb20fd2360dacda3eee429aba671fc022e2
Olivier Richard <olivier.richard@imag.fr>
2020-01-08 11:11:36 +0100

    cigri: add oar (wip)

--- cigri/arion-compose.nix
+++ cigri/arion-compose.nix
@@ -1,6 +1,8 @@
 { pkgs, lib,... }:
 let
 
+  inherit (import ./ssh-keys.nix pkgs) snakeOilPrivateKey snakeOilPublicKey;
+
 common = {
   
   service.volumes = [ "${builtins.getEnv "PWD"}:/srv" ];
@@ -17,19 +19,24 @@ common = {
     
     environment.systemPackages = with pkgs; [ telnet ruby ];
     imports = lib.attrValues pkgs.nur.repos.kapack.modules;
+    
+    # oar user's key files
+    environment.etc."privkey.snakeoil" = { mode = "0600"; source = snakeOilPrivateKey; };
+    environment.etc."pubkey.snakeoil" = { mode = "0600"; source = snakeOilPublicKey; };
 
-    services.cigri = {
+    services.oar = {
+      # oar db passwords
       database = {
-        host = "cigri";
-        passwordFile = "/srv/cigri-dbpassword";
+        host = "server";
+        passwordFile = "/srv/oar-dbpassword";
       };
-      server = {
-        host = "cigri";
-        logfile = "/tmp/cigri.log";
-      };
-    };
+      server.host = "server";
+      privateKeyFile = "/etc/privkey.snakeoil";
+      publicKeyFile = "/etc/pubkey.snakeoil";
+    };  
+    
   };
- 
+
 };
 
 addCommon = x: lib.recursiveUpdate x common;
@@ -40,10 +47,61 @@ in
   services.cigri = addCommon {
     service.hostname="cigri";
     nixos.configuration = {
-      services.cigri.dbserver.enable = true;
-      services.cigri.client.enable = true;
-      services.cigri.server.enable = true;
-      services.cigri.server.web.enable = true;
+      services.cigri = {
+        dbserver.enable = true;
+        client.enable = true;
+        database = {
+          host = "cigri";
+          passwordFile = "/srv/cigri-dbpassword";
+        };
+        server = {
+          enable = true;
+          web.enable = true;
+          host = "cigri";
+          logfile = "/tmp/cigri.log";
+        };        
+      };
+    }; 
+  };
+
+  services.frontend = addCommon {
+    service.hostname="frontend";
+    nixos.configuration = {
+      services.oar.client.enable = true;
+    };    
+  };
+
+  services.server = addCommon {
+    service.hostname="server";
+    nixos.configuration = {
+      services.oar.server.enable = true;
+      services.oar.dbserver.enable = true;
+    };
+  };
+  
+  services.node1 = addCommon {
+    service.hostname="node1";
+    nixos.configuration = {
+      services.oar.node = {
+        enable = true;
+        register = {
+          enable = true;
+          extraCommand = "/srv/prepare_oar_cgroup.sh init";
+        };
+      };
+    };
+  };
+  
+  services.node2 = addCommon {
+    service.hostname="node2";
+    nixos.configuration = {
+      services.oar.node = {
+        enable = true;
+        register = {
+          enable = true;
+          extraCommand = "/srv/prepare_oar_cgroup.sh init";
+        };
+      };
     };
   };
 }
