efd62a04c731eb2d130b63acb26cb7c0fb79ed6d
Olivier Richard <olivier.richard@imag.fr>
2020-02-17 15:57:52 +0100

    [simple] set nur-kapack rev/sha256

--- simple/arion-pkgs.nix
+++ simple/arion-pkgs.nix
@@ -4,12 +4,12 @@ import <nixpkgs> {
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
       repoOverrides = {
-        kapack = import /home/auguste/dev/nur-kapack {};
-        # kapack = import (pkgs.fetchgit {
-        #   url = https://github.com/oar-team/nur-kapack;
-        #   rev = "6a488adc04f98ac931e6277095b1a22a40f6839a";
-        #   sha256 = "0al83rgj2197ank1vy2799bc3wgpbjzcscdr5021frg5x479s0qj";
-        #   }) {};
+        #kapack = import /home/auguste/dev/nur-kapack {};
+        kapack = import (pkgs.fetchgit {
+          url = https://github.com/oar-team/nur-kapack;
+          rev = "bfb4dbd537e386a28158f3e71427873f651a6bc9";
+          sha256 = "1b5m0lhij4b8bwy2mg8qdw0djn72gcq5g4wp7imb815df9lmd2h9";
+        }) {};
       };
     };
   };
