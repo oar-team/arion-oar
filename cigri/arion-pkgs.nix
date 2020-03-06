4fd72279e5c59720b9e726475387baa166b7d302
Olivier Richard <olivier.richard@imag.fr>
2020-03-06 15:04:55 +0100

    update nur-kapack

--- cigri/arion-pkgs.nix
+++ cigri/arion-pkgs.nix
@@ -1,14 +1,15 @@
 # Instead of pinning Nixpkgs, we can opt to use the one in NIX_PATH
 import <nixpkgs> {
+  #overlays = [ (import /home/auguste/dev/nur-kapack/overlays/overlay-dev.nix) ];
   config.packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
       repoOverrides = {
         #kapack = import /home/auguste/dev/nur-kapack {};
         kapack = import (pkgs.fetchgit {
-          url = https://github.com/oar-team/nur-kapack;
-          rev = "bfb4dbd537e386a28158f3e71427873f651a6bc9";
-          sha256 = "1b5m0lhij4b8bwy2mg8qdw0djn72gcq5g4wp7imb815df9lmd2h9";
+           url = https://github.com/oar-team/nur-kapack;
+           rev = "521a345c64428a8fc7301b792987cda453074c57";
+           sha256 = "0dw3w3wq5c35hhb5cnhb36bcnp5dsc3brfvx92pa9f9hv1qxwx3d";
         }) {};
       };
     };
