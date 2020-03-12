1b54e41294f42647d75977bd3dc990750eb07ef8
Olivier Richard <olivier.richard@imag.fr>
2020-03-12 17:17:20 +0100

    NUR repoOverrides need to inherit pkgs

--- basic/arion-pkgs.nix
+++ basic/arion-pkgs.nix
@@ -10,7 +10,7 @@ import <nixpkgs> {
            url = https://github.com/oar-team/nur-kapack;
            rev = "521a345c64428a8fc7301b792987cda453074c57";
            sha256 = "0dw3w3wq5c35hhb5cnhb36bcnp5dsc3brfvx92pa9f9hv1qxwx3d";
-        }) {};
+        }) {inherit pkgs;};
       };
     };
   };
